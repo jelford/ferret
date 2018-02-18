f8c9c0a9a84b4bfc1e342684af3377d054f94434
James Elford <james.p.elford@gmail.com>
2018-02-18 16:37:22 +0000

    sdlc(release): update release script to run a flit build before bumpversion to avoid spurious "releases" being done locally

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@
 set -e
 
 export SOURCE_DATE_EPOCH=$(date +%s)
-bumpversion --message "Releasing version {new_version} with SOURCE_DATE_EPOCH=${SOURCE_DATE_EPOCH}" $1 
 flit build
+bumpversion --message "Releasing version {new_version} with SOURCE_DATE_EPOCH=${SOURCE_DATE_EPOCH}" $1 
 flit publish
